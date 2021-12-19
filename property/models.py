1b6c046dee8de55a90b44cc4f6bb5c3009abdfa3
Max Kutovoy <maxkutovoy@yandex.ru>
2021-12-19 21:51:58 +0600

    Удалил лишние поля из модели Flat

--- property/models.py
+++ property/models.py
@@ -5,16 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200, db_index=True)
-    owners_phonenumber = models.CharField(
-        'Номер владельца',
-        max_length=20,
-        db_index=True)
-    owner_pure_phone = PhoneNumberField(
-        'Нормализованный номер владельца',
-        blank=True,
-        region='RU')
-
     description = models.TextField('Текст объявления', blank=True)
     price = models.IntegerField('Цена квартиры', db_index=True)
 
