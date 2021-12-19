83d4dfc18c7da63cb53feab7c72cbbdbe7ca6f47
Max Kutovoy <maxkutovoy@yandex.ru>
2021-12-19 21:47:44 +0600

    Добавил связь собственников и квартиры

--- property/models.py
+++ property/models.py
@@ -5,8 +5,11 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    owner = models.CharField('ФИО владельца', max_length=200, db_index=True)
+    owners_phonenumber = models.CharField(
+        'Номер владельца',
+        max_length=20,
+        db_index=True)
     owner_pure_phone = PhoneNumberField(
         'Нормализованный номер владельца',
         blank=True,
@@ -88,12 +91,16 @@ class Claim(models.Model):
 
 
 class Owner(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    owner = models.CharField('ФИО владельца', max_length=200, db_index=True)
+    owners_phonenumber = models.CharField(
+        'Номер владельца',
+        max_length=20,
+        db_index=True)
     owner_pure_phone = PhoneNumberField(
         'Нормализованный номер владельца',
         blank=True,
-        region='RU'
+        region='RU',
+        db_index=True
     )
     own_flats = models.ManyToManyField(
         Flat,
@@ -101,3 +108,5 @@ class Owner(models.Model):
         related_name='flat_owners'
     )
 
+    def __str__(self):
+        return self.owner
