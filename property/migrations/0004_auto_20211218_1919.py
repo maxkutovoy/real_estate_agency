fcd07c0b9f5464f5a04ebf301b5fb898ac9c4ba3
Max Kutovoy <maxkutovoy@yandex.ru>
2021-12-30 10:30:20 +0600

    Оптимизировал конструкцию if

--- property/migrations/0004_auto_20211218_1919.py
+++ property/migrations/0004_auto_20211218_1919.py
@@ -7,12 +7,8 @@ def set_construction_status(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
 
     for flat in Flat.objects.all():
-        if flat.construction_year >= 2015:
-            flat.new_building = True
-            flat.save()
-        else:
-            flat.new_building = False
-            flat.save()
+        flat.new_building = flat.construction_year >= 2015
+        flat.save()
 
 
 class Migration(migrations.Migration):
